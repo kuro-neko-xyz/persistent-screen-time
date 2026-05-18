deef83b73ae8f3730c16da95dea163a876900e7d
Alejandro Alemán Hernández <alejandro@192.168.1.54>
2026-05-17 23:03:13 -0600

    Closes #5

--- seed.js
+++ seed.js
@@ -42,7 +42,67 @@ async function seedDatabase() {
     );
   `);
 
-  await client.end();
+  console.log("Reading file...");
+
+  const rawIphoneData = fs.readFileSync("iphone_usage.json");
+  const iphone = JSON.parse(rawIphoneData);
+
+  console.log(`Found ${iphone.length} records. Inserting...`);
+
+  const insertQuery = `
+    INSERT INTO events (init_time, end_time, device_uuid, app_id)
+    VALUES ($1, $2, $3, $4)
+    ON CONFLICT (init_time, device_uuid, app_id) DO NOTHING
+  `;
+
+  await client.query("BEGIN");
+
+  try {
+    for (const device of iphone) {
+      const deviceUUID = device.device_id;
+
+      try {
+        await client.query(
+          `
+            INSERT INTO devices(uuid)
+            VALUES ($1)
+            ON CONFLICT (uuid) DO NOTHING
+          `,
+          [deviceUUID],
+        );
+      } catch (error) {
+        console.error("Error inserting data:", error);
+      }
+
+      for (const event of device.events) {
+        const initTime = event.timestamp;
+        const endTime = new Date(
+          Date.parse(event.timestamp) + event.duration_seconds * 1000,
+        ).toISOString();
+        const appID = event.data.app;
+        const appName = event.data.title;
+
+        await client.query(
+          `
+            INSERT INTO apps(id, name)
+            VALUES ($1, $2)
+            ON CONFLICT (id) DO NOTHING
+          `,
+          [appID, appName],
+        );
+
+        await client.query(insertQuery, [initTime, endTime, deviceUUID, appID]);
+      }
+
+      await client.query("COMMIT");
+      console.log("Database seeded successfully.");
+    }
+  } catch (error) {
+    await client.query("ROLLBACK");
+    console.error("Error inserting data:", error);
+  } finally {
+    await client.end();
+  }
 }
 
 seedDatabase();
